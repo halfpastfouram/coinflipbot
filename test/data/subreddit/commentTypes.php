3eae74fb477be029cd8d4a3ffcbdcbc848c3508d
Bob <bobkruithof@gmail.com>
2016-11-06 22:13:16 +0100

    Version alpha-1.15
    - More refactoring.
    - A little bit more progress moving data-source related logic to the mappers.
    
    Todo:
    
    - Finish and fully implement subreddit and message services.
    - Create more unit tests.
    - Finetune continuous integration.

---
+++ test/data/subreddit/commentTypes.php
@@ -0,0 +1,10 @@
+<?php
+return [
+	0 => \Coinflipbot\ParseType::FLIP,
+	1 => \Coinflipbot\ParseType::FLIP,
+	2 => \Coinflipbot\ParseType::WHITELIST,
+	3 => \Coinflipbot\ParseType::WHITELISTED_FLIP,
+	4 => \Coinflipbot\ParseType::UNWHITELIST,
+	5 => \Coinflipbot\ParseType::WHITELISTED_FLIP,
+	6 => \Coinflipbot\ParseType::FLIP,
+];
